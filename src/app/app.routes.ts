3265cb2cdc5ea91d7d2d877983b2eae2d2baff00
NeoRise456 <neorise456@gmail.com>
2024-09-28 17:57:21 -0500

    feat(routes): added new routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-
+import {DashboardComponent} from "./wallet/pages/dashboard/dashboard.component";
 
 export const routes: Routes = [
-
+  { path: 'dashboard', component: DashboardComponent },
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**',  redirectTo: 'dashboard' , pathMatch: 'full'},
 ];
