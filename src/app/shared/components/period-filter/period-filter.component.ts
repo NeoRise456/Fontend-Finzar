3cddce9f0e1e4817929798a80a2c61c52a90ec64
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:38:38 -0500

    feat(period-filter): date picker implementation.

--- src/app/shared/components/period-filter/period-filter.component.ts
+++ src/app/shared/components/period-filter/period-filter.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+
 
 @Component({
   selector: 'app-period-filter',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatDatepickerModule],
   templateUrl: './period-filter.component.html',
   styleUrl: './period-filter.component.css'
 })
