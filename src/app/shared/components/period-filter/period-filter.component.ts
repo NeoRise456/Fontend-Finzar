8cad90efd34d7940c8055d1bf9070e99ab169b04
NeoRise456 <neorise456@gmail.com>
2024-09-28 22:03:57 -0500

    feat(period-filter): implemented date picker.

--- src/app/shared/components/period-filter/period-filter.component.ts
+++ src/app/shared/components/period-filter/period-filter.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDatepickerModule} from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-period-filter',
   standalone: true,
-  imports: [MatFormFieldModule, MatDatepickerModule],
+  imports: [MatFormFieldModule, MatDatepickerModule,MatNativeDateModule,ReactiveFormsModule],
   templateUrl: './period-filter.component.html',
   styleUrl: './period-filter.component.css'
 })
 export class PeriodFilterComponent {
+  dateRangeForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.dateRangeForm = this.fb.group({
+      start: [new Date(new Date().getFullYear(), new Date().getMonth(), 1)], // Default start date
+      end: [new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)]    // Default end date
+    });
+  }
 }
