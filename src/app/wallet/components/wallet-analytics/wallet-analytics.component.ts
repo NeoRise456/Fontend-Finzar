5142dc69598b6320dd940ec682c6ed77d58fc80c
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:35:29 -0500

    feat(wallet-analytics): component setup.

---
+++ src/app/wallet/components/wallet-analytics/wallet-analytics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-analytics',
+  standalone: true,
+  imports: [],
+  templateUrl: './wallet-analytics.component.html',
+  styleUrl: './wallet-analytics.component.css'
+})
+export class WalletAnalyticsComponent {
+
+}
