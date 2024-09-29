5b95f69495042eea4dc6110c2141cca37e2b2dc8
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:35:20 -0500

    feat(wallet-filters): component setup.

---
+++ src/app/wallet/components/wallet-filters/wallet-filters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './wallet-filters.component.html',
+  styleUrl: './wallet-filters.component.css'
+})
+export class WalletFiltersComponent {
+
+}
