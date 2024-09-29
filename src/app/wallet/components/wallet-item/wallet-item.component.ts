811e165595e15860b697f6ac0a70d8542916ee6c
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:33:20 -0500

    feat(wallet-item): added wallet item component implementation.

---
+++ src/app/wallet/components/wallet-item/wallet-item.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+import {Wallet} from "../../models/wallet.entity";
+
+@Component({
+  selector: 'app-wallet-item',
+  standalone: true,
+  imports: [MatCardModule, RouterLink,MatIconModule],
+  templateUrl: './wallet-item.component.html',
+  styleUrl: './wallet-item.component.css'
+})
+export class WalletItemComponent {
+  @Input() wallet!: Wallet;
+}
