750c9612c383f6173bb69fde8d46e72a7a14380a
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:38:15 -0500

    feat(wallet-overview): component setup.

---
+++ src/app/wallet/components/wallet-overview/wallet-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './wallet-overview.component.html',
+  styleUrl: './wallet-overview.component.css'
+})
+export class WalletOverviewComponent {
+
+}
