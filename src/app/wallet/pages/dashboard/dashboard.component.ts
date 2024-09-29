c1b0650a112c50ff7016991a9b4e9828f151801a
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:34:46 -0500

    feat(dashboard): implementation of wallet components.

--- src/app/wallet/pages/dashboard/dashboard.component.ts
+++ src/app/wallet/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {BaseService} from "../../../shared/services/base.service.service";
+import {Wallet} from "../../models/wallet.entity";
+import {WalletListComponent} from "../../components/wallet-list/wallet-list.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ WalletListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  userWallets!: Wallet[];
+
+  constructor(private baseService: BaseService<Wallet>) {
+    this.userWallets = [];
+  }
+
+  ngOnInit(): void {
+    this.baseService.getUserWallets(1).subscribe(
+      wallets => {
+        this.userWallets = wallets;
+      }
+    );
+  }
 
 }
