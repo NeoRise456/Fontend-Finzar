7df1609eb4bd813ac74115a1b094ace20c235f3b
NeoRise456 <neorise456@gmail.com>
2024-09-28 20:35:48 -0500

    feat(period-filter): component setup.

---
+++ src/app/shared/components/period-filter/period-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-period-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './period-filter.component.html',
+  styleUrl: './period-filter.component.css'
+})
+export class PeriodFilterComponent {
+
+}
