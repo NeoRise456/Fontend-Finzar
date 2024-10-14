4565badb5b32edaf1cec536cf158d50936aaa8cd
NeoRise456 <neorise456@gmail.com>
2024-10-13 21:00:38 -0500

    feat(wallet): added earning entity.

---
+++ src/app/wallet/model/earning.entity.ts
@@ -0,0 +1,28 @@
+
+export class Earning {
+  id: number;
+  walletId: number;
+  amount: number;
+  category_id: number;
+  note: string;
+  date: string;
+  recurrent_id: number;
+
+  constructor(
+    id = 0,
+    walletId = 0,
+    amount = 0,
+    category_id = 0,
+    note = '',
+    date = '',
+    recurrent_id = 0
+  ) {
+    this.id = id;
+    this.walletId = walletId;
+    this.amount = amount;
+    this.category_id = category_id;
+    this.note = note;
+    this.date = date;
+    this.recurrent_id = recurrent_id;
+  }
+}
