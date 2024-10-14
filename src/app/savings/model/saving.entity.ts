0316188d46931a97b7217b326c2140e281479329
NeoRise456 <neorise456@gmail.com>
2024-10-13 21:00:59 -0500

    feat(saving): added saving entity.

--- src/app/savings/model/saving.entity.ts
+++ src/app/savings/model/saving.entity.ts
@@ -4,34 +4,23 @@ export class Saving {
   id: number;
   userId: number;
   name: string;
-  totalGoal: number;
-  currentGoal: number;
+  totalGoal: number ;
   currentAmount: number;
   categoryId: number;
-  recurringId: number;
-  startDate: Date;
-  endDate: Date;
   constructor(
     id = 0,
     userId = 0,
     name = '',
     totalGoal = 0,
-    currentGoal = 0,
     currentAmount = 0,
     categoryId = 0,
     recurringId = 0,
-    startDate = new Date(),
-    endDate = new Date()
   ) {
     this.id = id;
     this.userId = userId;
     this.name = name;
     this.totalGoal = totalGoal;
-    this.currentGoal = currentGoal;
     this.currentAmount = currentAmount;
     this.categoryId = categoryId;
-    this.recurringId = recurringId;
-    this.startDate = startDate;
-    this.endDate = endDate;
   }
 }
