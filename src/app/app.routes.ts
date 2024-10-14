96aad9fcd867dc9bca6e1cd0309fd696176276a3
NeoRise456 <neorise456@gmail.com>
2024-10-13 20:59:11 -0500

    feat(routes): added budget and saving routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from "./wallet/pages/dashboard/dashboard.component";
 import {WalletViewComponent} from "./wallet/pages/wallet-view/wallet-view.component";
 import {SavingsDashboardComponent} from "./savings/pages/savings-dashboard/savings-dashboard.component";
-import {BudgetsDashboardComponent} from "./savings/pages/budgets-dashboard/budgets-dashboard.component";
+import {BudgetsDashboardComponent} from "./budgets/pages/budgets-dashboard/budgets-dashboard.component";
 import {SavingsDetailComponent} from "./savings/pages/savings-detail/savings-detail.component";
 
 export const routes: Routes = [
