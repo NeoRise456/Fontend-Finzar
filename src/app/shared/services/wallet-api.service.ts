7c033d4f9d8290e715d383985f3371fd050ae7cb
NeoRise456 <neorise456@gmail.com>
2024-10-15 17:02:51 -0500

    feat(wallet): added wallet api service.

---
+++ src/app/shared/services/wallet-api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Wallet} from "../../wallet/model/wallet.entity";
+import {BaseService} from "./base.service.service";
+import {catchError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WalletApiService extends BaseService<Wallet> {
+  resourceEndpoint: string = '/wallets';
+
+  //get wallet by wallet id
+  getWalletById(walletId: any) {
+    return this.http.get<Wallet>(`${this.basePath}${this.resourceEndpoint}/${walletId}`, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+}
