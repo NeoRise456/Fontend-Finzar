efb169dbf829d0fa25ad92b07595dad5d7a3b186
NeoRise456 <neorise456@gmail.com>
2024-10-15 17:04:39 -0500

    feat(savings): added savings service and entity.

--- src/app/savings/pages/savings-detail/savings-detail.component.ts
+++ src/app/savings/pages/savings-detail/savings-detail.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {BalancePiechartComponent} from "../../../wallet/components/balance-piechart/balance-piechart.component";
+import {MatCardModule} from "@angular/material/card";
+import {BalanceDisplayComponent} from "../../../wallet/components/balance-display/balance-display.component";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-savings-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    BalancePiechartComponent,
+    MatCardModule,
+    BalanceDisplayComponent,
+    MatIconButton,
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './savings-detail.component.html',
   styleUrl: './savings-detail.component.css'
 })
 export class SavingsDetailComponent {
+  titles = [
+    'Total Balance',
+    'Total Period Change',
+    'Total Period Expenses',
+    'Total Period Income',
+    'Period Income',
+    'Period Expenses',
+    'Saving asdf'
+  ];
+
+  balances = [
+    2000,
+    -123,
+    4000,
+    345
+  ];
+
+
 
 }
