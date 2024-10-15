4b2172faa815cc0cb01bdd9c7cc6f9e942846cf1
NeoRise456 <neorise456@gmail.com>
2024-10-15 17:04:10 -0500

    feat(wallet): added earnings and expenses entities and services to wallet view page.

--- src/app/wallet/pages/wallet-view/wallet-view.component.ts
+++ src/app/wallet/pages/wallet-view/wallet-view.component.ts
@@ -5,17 +5,57 @@ import {MatCardModule} from "@angular/material/card";
 import {MatTableModule} from "@angular/material/table";
 import {BalanceDisplayComponent} from "../../components/balance-display/balance-display.component";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import {EarningsApiService} from "../../services/earnings-api.service";
+import {ExpensesApiService} from "../../services/expenses-api.service";
+import {Earning} from "../../model/earning.entity";
+import {Expense} from "../../model/expense.entity";
+import {NgForOf} from "@angular/common";
+import {WalletItemComponent} from "../../components/wallet-item/wallet-item.component";
+import {ActivatedRoute} from "@angular/router";
+import {Wallet} from "../../model/wallet.entity";
+import {WalletApiService} from "../../../shared/services/wallet-api.service";
 
 @Component({
   selector: 'app-wallet-view',
   standalone: true,
   imports: [MatButtonModule, MatIconModule, MatCardModule, MatTableModule, BalanceDisplayComponent,
-  MatCheckboxModule],
+    MatCheckboxModule, NgForOf, WalletItemComponent, MatTableModule],
   templateUrl: './wallet-view.component.html',
   styleUrl: './wallet-view.component.css'
 })
 export class WalletViewComponent {
-  constructor( ) {
+  earnings: Earning[];
+  expenses: Expense[];
+  walletId: number;
+  wallet: Wallet;
+  cashflow!: any[];
+
+
+
+  displayedColumns: string[] = [ 'id', 'walletId', 'amount', 'category_id', 'note', 'date', 'recurrent_id'];
+
+  constructor( private earningApiService: EarningsApiService, private expensesApiService: ExpensesApiService, private route: ActivatedRoute, private walletApiService: WalletApiService) {
+    this.earnings = this.expenses = [];
+    this.walletId = 0;
+    this.wallet = new Wallet();
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.walletId = params['id'];});
+
+    this.walletApiService.getWalletById(this.walletId).subscribe(wallet => {
+      this.wallet = wallet;
+    });
+
+    this.earningApiService.getEarningsByWalletId(this.walletId).subscribe(earnings => {
+      this.earnings = earnings;
+    });
+    this.expensesApiService.getExpensesByWalletId(this.walletId).subscribe(expenses => {
+      this.expenses = expenses;
+    });
+
+    this.cashflow = this.expenses.concat(this.earnings);
   }
 
   titles = [
