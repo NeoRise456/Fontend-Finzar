0487a23daa47277deda9eb8d8901166b23569fad
NeoRise456 <neorise456@gmail.com>
2024-10-15 17:02:57 -0500

    feat(wallet): added transaction api service.

---
+++ src/app/shared/services/transaction-api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "./base.service.service";
+import {Transaction} from "../model/transaction.entity";
+import {catchError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionApiService extends BaseService<Transaction> {
+
+  resourceEndpoint: string = '/transactions';
+
+  //get transactions by user id
+  getTransactionsByUserId(userId: any) {
+    return this.http.get<Transaction[]>(`${this.basePath}${this.resourceEndpoint}?userId=${userId}`, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+
+}
