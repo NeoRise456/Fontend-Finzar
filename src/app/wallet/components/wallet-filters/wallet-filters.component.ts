8096eb06c915a379f29816793dac193ef40f6982
Pachieeee <sebaspacheco2003@gmail.com>
2024-10-23 17:35:18 -0500

    feat: added wallet filter functionality.

--- src/app/wallet/components/wallet-filters/wallet-filters.component.ts
+++ src/app/wallet/components/wallet-filters/wallet-filters.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatInputModule } from "@angular/material/input";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { Category } from "../../../shared/model/categories.entity";
+import { CategoryApiService } from "../../../shared/services/category-api.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-wallet-filters',
   standalone: true,
-  imports: [],
+  imports: [MatDatepickerModule, MatInputModule, MatNativeDateModule, MatFormFieldModule, MatCardModule, MatSelectModule, MatButtonModule, ReactiveFormsModule, NgForOf],
   templateUrl: './wallet-filters.component.html',
   styleUrl: './wallet-filters.component.css'
 })
-export class WalletFiltersComponent {
+export class WalletFiltersComponent implements OnInit{
+  categories: Category[] | undefined;
+  walletForms!: FormGroup;
 
+  constructor(private fb: FormBuilder, private categoryApiService: CategoryApiService) {
+    this.categories = [];
+  }
+
+  ngOnInit(): void {
+    this.walletForms = this.fb.group ({
+      category: null,
+      start: [new Date(new Date().getFullYear(), new Date().getMonth(), 1)],
+      end: [new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)]
+    });
+    this.categoryApiService.getAllCategories().subscribe(categories => {
+        this.categories = categories;
+    })
+  }
+
+  resetFilters(): void {
+    this.walletForms.reset({
+      start: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+      end: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
+    });
+  }
 }
