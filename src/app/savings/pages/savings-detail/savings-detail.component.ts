bafb3ee763c6ddbe72deaf32eefaf581006396c8
JosuePaiva02 <u202119095@upc.edu.pe>
2024-10-24 00:23:25 -0500

    feat: added saving edit functions to saving detail component

--- src/app/savings/pages/savings-detail/savings-detail.component.ts
+++ src/app/savings/pages/savings-detail/savings-detail.component.ts
@@ -4,13 +4,16 @@ import {MatCardModule} from "@angular/material/card";
 import {BalanceDisplayComponent} from "../../../wallet/components/balance-display/balance-display.component";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {SavingApiService} from "../../services/saving-api.service";
 import {Saving} from "../../model/saving.entity";
 import {SavingTransaction} from "../../model/saving-transaction.entity";
 import { MatTableModule } from '@angular/material/table';
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {DatePipe, NgIf} from "@angular/common";
+import {MatDialog} from "@angular/material/dialog";
+import {SavingDeleteComponent} from "../../components/saving-delete/saving-delete.component";
+import {SavingEditComponent} from "../../components/saving-edit/saving-edit.component";
 
 @Component({
   selector: 'app-savings-detail',
@@ -31,6 +34,7 @@ import {DatePipe, NgIf} from "@angular/common";
   ]
 })
 export class SavingsDetailComponent implements OnInit {
+
   titles = [
     'Savings Goal',
     'Saved so far',
@@ -55,7 +59,9 @@ export class SavingsDetailComponent implements OnInit {
 
   constructor(
       private route: ActivatedRoute,
-      private savingApiService: SavingApiService
+      private savingApiService: SavingApiService,
+      private router: Router,
+      private dialog: MatDialog
   ) {}
 
 
@@ -94,8 +100,45 @@ export class SavingsDetailComponent implements OnInit {
     }
   }
 
+    openDeleteDialog(): void {
+        const dialogRef = this.dialog.open(SavingDeleteComponent, {
+            data: { savingId: this.savingId }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                console.log('Saving deleted from dialog');
+                this.router.navigate(['/savings']);
+            }
+        });
+    }
+  openEditDialog(saving: Saving) {
+    const dialogRef = this.dialog.open(SavingEditComponent, {
+      data: saving
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.updateSaving(this.savingId, result);
+      }
+    });
+  }
+
+  updateSaving(id: number | undefined, saving: Saving) {
+    if (id !== undefined) {
+      this.savingApiService.updateSaving(id, saving).subscribe(
+          updatedSaving => {
+            console.log('Saving updated:', updatedSaving);
+            this.loadSaving();
+          },
+          error => {
+            console.error('Error updating saving:', error);
+          }
+      );
+    }
+  }
   get hasTransactions(): boolean {
     return this.savingTransactions && this.savingTransactions.length > 0;
   }
-
 }
+
