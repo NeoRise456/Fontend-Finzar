c472bdb32984ea70e93794ec7fa9ff53adcf41a8
pachieeee <sebaspacheco2003@gmail.com>
2024-11-21 12:17:51 -0500

    feat(app-routes): added app routes authentication guard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,14 @@ import {BudgetsDashboardComponent} from "./budgets/pages/budgets-dashboard/budge
 import {SavingsDetailComponent} from "./savings/pages/savings-detail/savings-detail.component";
 import {SignInComponent} from "./iam/pages/sign-in/sign-in.component";
 import {SignUpComponent} from "./iam/pages/sign-up/sign-up.component";
+import {authenticationGuard} from "./iam/services/authentication.guard";
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'wallet/:id', component: WalletViewComponent },
-  {path: 'savings', component: SavingsDashboardComponent},
-  {path: 'saving/:id', component: SavingsDetailComponent },
-  {path: 'budgets', component: BudgetsDashboardComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authenticationGuard] },
+  { path: 'wallet/:id', component: WalletViewComponent, canActivate: [authenticationGuard] },
+  {path: 'savings', component: SavingsDashboardComponent, canActivate: [authenticationGuard] },
+  {path: 'saving/:id', component: SavingsDetailComponent, canActivate: [authenticationGuard] },
+  {path: 'budgets', component: BudgetsDashboardComponent, canActivate: [authenticationGuard] },
   {path: 'sign-in', component: SignInComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: '', redirectTo: 'sign-in', pathMatch: 'full' },
