312ba2e1419c3b7f5e0120b1a46b9a07de6f576f
cwassointt <dillpixel1@gmail.com>
2024-11-21 18:31:44 -0500

    fix: fixed categories entity fields.

--- src/app/shared/model/categories.entity.ts
+++ src/app/shared/model/categories.entity.ts
@@ -1,12 +1,15 @@
 export class Category {
-  id:number;
-  name:string;
+  id: number;
+  name: string;
+  description: string;
 
   constructor(
-    id =0,
-    name =''
+      id = 0,
+      name = '',
+      description = ''
   ) {
     this.id = id;
     this.name = name;
+    this.description = description;
   }
 }
