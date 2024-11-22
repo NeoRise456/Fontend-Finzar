37732fc21e18e16b62cc30596880277ce5e750a9
pachieeee <sebaspacheco2003@gmail.com>
2024-11-21 23:54:17 -0500

    fix(wallet): fixed wallet entity fields.

--- src/app/wallet/model/wallet.entity.ts
+++ src/app/wallet/model/wallet.entity.ts
@@ -2,24 +2,17 @@ export class Wallet {
   id: number;
   name: string;
   balance: number;
-  user: {
-    userId: number;
-    username: string;
-  };
+  userId: number;
 
   constructor(
       id = 0,
       name = '',
       balance = 0,
       userId = 0,
-      username = ''
   ) {
     this.id = id;
     this.name = name;
     this.balance = balance;
-    this.user = {
-      userId: userId,
-      username: username,
-    };
+    this.userId = userId;
   }
 }
