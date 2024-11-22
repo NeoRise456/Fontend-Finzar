45e7fe8edcb44a6c7a32d9a603684f2fbe8c13d6
pachieeee <sebaspacheco2003@gmail.com>
2024-11-21 19:29:52 -0500

    fix(sign-in): fixed sign-in component to work properly with new authentication.

--- src/app/iam/pages/sign-in/sign-in.component.ts
+++ src/app/iam/pages/sign-in/sign-in.component.ts
@@ -1,31 +1,43 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
-import {Router} from "@angular/router";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {SignInRequest} from "../../model/sign-in.request";
+import {AuthenticationService} from "../../services/authentication.service";
+import {BaseFormComponent} from "../../../shared/components/base-form.component";
+import {NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [FormsModule, MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, NgIf, RouterLink],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent {
-  private _formBuilder = inject(FormBuilder);
-  private _router = inject(Router)
+export class SignInComponent extends BaseFormComponent implements OnInit {
 
-  signInFormGroup = this._formBuilder.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required]
-  });
+  signInFormGroup!: FormGroup;
+  submitted = false;
 
-  signIn() {
-    // Implement sign in
+  constructor(private builder: FormBuilder, private authenticationService: AuthenticationService) {
+    super();
   }
 
-  redirectSignIn() {
-    this._router.navigate(['/sign-up']);
+  ngOnInit() {
+    this.signInFormGroup = this.builder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signIn() {
+    if (this.signInFormGroup.invalid) return;
+    let username = this.signInFormGroup.value.username;
+    let password = this.signInFormGroup.value.password;
+    const signInRequest = new SignInRequest(username, password);
+    this.authenticationService.signIn(signInRequest);
+    this.submitted = true;
   }
 }
