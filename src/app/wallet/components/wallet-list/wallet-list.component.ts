9b4672d7b3e0f6f2767eacb764788c2d031f0f49
pachieeee <sebaspacheco2003@gmail.com>
2024-11-21 23:54:44 -0500

    fix(wallet): fixed wallet creation.

--- src/app/wallet/components/wallet-list/wallet-list.component.ts
+++ src/app/wallet/components/wallet-list/wallet-list.component.ts
@@ -7,6 +7,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {CreateWalletDialogComponent} from "../create-wallet-dialog/create-wallet-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
 import {BaseService} from "../../../shared/services/base.service.service";
+import {AuthenticationService} from "../../../iam/services/authentication.service";
 
 @Component({
   selector: 'app-wallet-list',
@@ -22,28 +23,32 @@ import {BaseService} from "../../../shared/services/base.service.service";
 })
 export class WalletListComponent {
   @Input() wallets!: Wallet[];
+  currentUserId: number = 0;
+  currentUsername: string = '';
 
-  constructor(private dialog: MatDialog,private baseService: BaseService<Wallet>) {
+  constructor(private dialog: MatDialog,private baseService: BaseService<Wallet>, private authenticationService: AuthenticationService) {
   }
 
   OnAddWallet(): void {
     const dialogRef = this.dialog.open(CreateWalletDialogComponent, {
       hasBackdrop: true
     });
-
+    this.authenticationService.currentUserId.subscribe(userId => {
+        this.currentUserId = userId;
+    })
+    this.authenticationService.currentUsername.subscribe(username => {
+        this.currentUsername = username
+    })
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
 
-
         this.baseService.createWallet(
-          new Wallet(
-            this.generateUniqueId(),
-            result.walletName,
-            1,
-            result.initialBalance,
-            result.initialBalance
-          )
+          {
+            userId: this.currentUserId,
+             name: result.walletName,
+            initialBalance: result.initialBalance
+          }
         ).subscribe({
           next: (wallet) => {
             this.wallets.push(wallet); // Add the new wallet to the array
